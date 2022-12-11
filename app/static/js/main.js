2d7cfd13a49ebe6ad770b8eeb2bc8087b6212008
Vahe <v.serdjanian@gmail.com>
2022-12-11 17:07:47 +0100

    static games page added

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,1 +1,16 @@
-console.log("hello world")
+const addNewGameBtn = document.getElementById("addNewGameBtn")
+const homeBtn = document.getElementById("homeBtn")
+const profileBtn = document.getElementById("profileBtn")
+
+addNewGameBtn.addEventListener("click",(e)=>{
+	e.preventDefault()
+	console.log("new game added")
+})
+homeBtn.addEventListener("click",(e)=>{
+	e.preventDefault()
+	console.log("home")
+})
+profileBtn.addEventListener("click",(e)=>{
+	e.preventDefault()
+	console.log("profile")
+})
