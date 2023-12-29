24f0b79fb199f5606fad0700ffe0ee281611f420
GreatPy <v.serdjanian@gmail.com>
2023-12-29 18:52:51 +0100

    fall back screen added on wishlist screen

--- frontend_vahouche/weeple/lib/widgets/navigation/wishlist.dart
+++ frontend_vahouche/weeple/lib/widgets/navigation/wishlist.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:weeple/styles.dart';
 
 class Wishlist extends StatelessWidget {
   const Wishlist({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Wishlist"));
+    return const Padding(
+      padding: EdgeInsets.only(left: 8, right: 8, top: 16),
+      child: Column(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                """Aucun jeu n'est sur votre wishlist 
+pour le moment
+vous pouvez en ajouter grâce au bouton""",
+                style: TextStyle(
+                    color: primaryColor,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold),
+              ),
+              Icon(
+                Icons.add,
+                size: 36,
+                color: primaryColor,
+              )
+            ],
+          ),
+          Spacer(),
+          Image(
+            image: AssetImage("assets/empty.webp"),
+            width: 300,
+          ),
+          SizedBox(height: 64),
+        ],
+      ),
+    );
   }
 }
